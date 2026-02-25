4fb7466fafb11298d3444abb701aa493e86bd8bd
Yves-Tabor <walvesoutis.gmail.com>
2026-02-25 10:37:00 +0200

    ..

--- override.ts
+++ override.ts
@@ -1,25 +1,43 @@
-interface Name{
-    readonly name: string,
-    other: string;
-    getName:()=> string
+// class Person{
+//     name: string;
+//     other: unknown;
+//     id: number = 1;
+//     constructor(name: string, other: unknown){
+//         this.name = name;
+//         this.other = other;
+//     }
+//     getId(): void {
+//         console.log("Subject " + this.id);
+//     }
+// }
+
+// class Enroll extends Person{
+//         getId(): string{
+//             super.getId();
+//             if(typeof this.other === "number" && this.other > 18){
+//             return "Succefully enrolled\nName: " + this.name + "  Age: " + this.other;
+//             }
+//             else{
+//                 return "You are not eligible for enrollment";
+//             }
+//         }
+// }
+// const person = new Enroll("John", 23);
+// console.log(person.getId());
+
+class Person {
+  constructor(public name: string = "Walves") {}
+  getname(): void{
+    console.log(this.name);
+  }
+}
+class Student extends Person {
+  override name: string;
+    constructor(name: string){
+        super(name);
+        this.name = name.toUpperCase();
+    }
 }
 
-class Person implements Name{
-    name: string;
-    other: string;
-    constructor(name: string, other: string){
-        this.name = name;
-        this.other = other;
-    }
-    getName(): string {
-        return "My name is " + this.name;
-    }
-}
-
-class Enroll extends Person{
-    constructor(name: string, other: string){
-        super(name, other);
-    }
-}
-const person = new Person("John", "Doe");
-console.log(person.getName());
+new Student("John").getname();
+new Person().getname();
